32ca107a76ec66f95967e76f0225b6842a33ac04
rahmanfaslu <rahmanfaslu33@gmail.com>
2026-01-29 00:00:53 +0530

    after live backend link

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,5 +1,5 @@
 export const createUser = async (userData) => {
-  const res = await fetch("http://localhost:5000/api/users", {
+  const res = await fetch("https://shigify-backend.onrender.com/api/users", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(userData)
